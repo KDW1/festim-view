10ee0d493080f15b76e760b75792f2cf894a33a7
kdw1 <diorrw@gmail.com>
2026-06-24 10:42:10 -0500

    Added the StateLoader from the Trame tutorials

---
+++ python/trame/SimpleCone.py
@@ -0,0 +1,86 @@
+from trame.app import TrameApp
+from trame.ui.vuetify3 import SinglePageLayout
+from trame.widgets import vuetify3 as v3
+from trame.widgets import paraview
+from trame.decorators import change
+
+from paraview import simple
+
+DEFAULT_RESOLUTION = 6
+
+# -----------------------------------------------------------------------------
+# trame setup
+# -----------------------------------------------------------------------------
+
+class ConeApp(TrameApp):
+    def __init__(self, server=None):   
+        super().__init__(server)
+
+        self._init_paraview()
+        self._build_ui()
+
+# -----------------------------------------------------------------------------
+# ParaView code
+# -----------------------------------------------------------------------------
+
+    def _init_paraview(self):
+        self.cone = simple.Cone()
+        self.representation = simple.Show(self.cone)
+        self.view = simple.Render()
+
+
+    @change("resolution")
+    def update_cone(self, resolution, **_kwargs):
+        self.cone.Resolution = resolution
+        self.ctrl.view_update()
+
+
+    def update_reset_resolution(self):
+        self.state.resolution = DEFAULT_RESOLUTION
+
+
+# -----------------------------------------------------------------------------
+# GUI
+# -----------------------------------------------------------------------------
+
+    def _build_ui(self):
+        self.state.trame__title = "ParaView cone"
+
+        with SinglePageLayout(self.server) as self.ui:
+            self.ui.icon.click = self.ctrl.view_reset_camera
+            self.ui.title.set_text("Cone Application")
+
+            with self.ui.toolbar:
+                v3.VSpacer()
+                v3.VSlider(
+                    v_model=("resolution", DEFAULT_RESOLUTION),
+                    min=3,
+                    max=60,
+                    step=1,
+                    hide_details=True,
+                    density="compact",
+                    style="max-width: 300px",
+                )
+                v3.VDivider(vertical=True, classes="mx-2")
+                v3.VBtn(icon="mdi-undo-variant", click=self.update_reset_resolution)
+
+            with self.ui.content:
+                with v3.VContainer(
+                    fluid=True,
+                    classes="pa-0 fill-height",
+                ):
+                    html_view = paraview.VtkRemoteView(self.view)
+                    # html_view = paraview.VtkLocalView(self.view)
+                    self.ctrl.view_update = html_view.update
+                    self.ctrl.view_reset_camera = html_view.reset_camera
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+def main():
+    app = ConeApp()
+    app.server.start()
+
+if __name__ == "__main__":
+    main()
---
+++ python/trame/StateLoader.py
@@ -0,0 +1,99 @@
+from trame.app import TrameApp
+from trame.ui.vuetify3 import SinglePageLayout
+from trame.widgets import vuetify3 as v3
+from trame.widgets import paraview, client
+
+from pathlib import Path
+
+from paraview import simple
+
+import asyncio
+
+def get_or_create_eventloop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
+        else:
+            raise ex
+        
+get_or_create_eventloop()
+# -----------------------------------------------------------------------------
+# Trame setup
+# -----------------------------------------------------------------------------
+
+class StateLoaderApp(TrameApp):
+
+    def __init__(self, server=None):
+        super().__init__(server)
+        self.server.cli.add_argument("--data", help="Path to state file", dest="data")
+
+        # Preload paraview modules onto server
+        paraview.initialize(self.server)
+
+        self.ctrl.on_server_ready.add(self.load_data)
+        self._build_ui()
+
+
+# -----------------------------------------------------------------------------
+# ParaView code
+# -----------------------------------------------------------------------------
+
+
+    def load_data(self, **_kwargs):
+        # CLI
+        args, _ = self.server.cli.parse_known_args()
+
+        full_path = str(Path(args.data).resolve().absolute())
+        working_directory = str(Path(args.data).parent.resolve().absolute())
+
+        # ParaView
+        simple.LoadState(
+            full_path,
+            data_directory=working_directory,
+            restrict_to_data_directory=True,
+        )
+        self.view = simple.GetActiveView()
+        self.view.MakeRenderWindowInteractor(True)
+        simple.Render(self.view)
+
+        # HTML
+        with SinglePageLayout(self.server) as self.ui:
+            self.ui.icon.click = self.ctrl.view_reset_camera
+            self.ui.title.set_text("ParaView State Viewer")
+
+            with self.ui.content:
+                with v3.VContainer(fluid=True, classes="pa-0 fill-height"):
+                    html_view = paraview.VtkRemoteView(self.view)
+                    self.ctrl.view_reset_camera = html_view.reset_camera
+                    self.ctrl.view_update = html_view.update 
+
+# -----------------------------------------------------------------------------
+# GUI
+# -----------------------------------------------------------------------------
+
+    def _build_ui(self):
+        self.state.trame__title = "State Viewer"
+
+        with SinglePageLayout(self.server) as self.ui:
+            self.ui.icon.click = self.ctrl.view_reset_camera
+            self.ui.title.set_text("ParaView State Viewer")
+
+            with self.ui.content:
+                with v3.VContainer(fluid=True, classes="pa-0 fill-height"):
+                    client.Loading("Loading state")
+
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+def main():
+    app = StateLoaderApp()
+    app.server.start()
+
+if __name__ == "__main__":
+    main()

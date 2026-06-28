2af57360b9de571b5294053abd77860bb3bea195
kdw1 <diorrw@gmail.com>
2026-06-27 22:50:23 -0500

    Printing the associated attributes and variables associated with the .bp file, intending to do something with vtk.xml file

--- python/read_my_bp.py
+++ python/read_my_bp.py
@@ -1,12 +1,20 @@
 import os
 from adios2 import FileReader
 
-with FileReader("../out/field_export.bp") as s:
+print(os.path.join(os.getcwd(), "out/field_export.bp"))
+with FileReader(os.path.join(os.getcwd(), "out/field_export.bp")) as s:
     # inspect variables
     vars = s.available_variables()
+    attributes = s.available_attributes()
+    # print("Variable Name: ", vars)
     for name, info in vars.items():
-        print("variable_name: " + name, end=" ")
+        print("\nvariable_name: " + name, end=" ")
         for key, value in info.items():
             print("\t" + key + ": " + value, end=" ")
-        print()
+    print(f"There are {len(attributes.keys())} attributes available: ", attributes)
     print()
+    for variable_of_interest in vars:
+        steps = int(vars[variable_of_interest]["AvailableStepsCount"])
+        # print("\n\n")
+        data_of_interest = s.read(variable_of_interest, [0, steps])
+        print(f"Data of interest, {variable_of_interest}\n", data_of_interest.tolist())
